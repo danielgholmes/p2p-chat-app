b9cf3749149e748084eed3677c6b16cf32a5890e
JonGerrand <gerrand.jonathan@gmail.com>
2015-04-13 14:10:39 +0200

    First attempt at wxPython GUI

---
+++ src/GUI/GUI_trial.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 13:30:37 2015
+
+@author: Jonathan Gerrand
+"""
+
+import wx
+
+app = wx.App()
+
+frame = wx.Frame(None, -1, 'GUI_trial.py')
+frame.Show()
+
+app.MainLoop()
